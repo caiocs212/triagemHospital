5dcbb9928e7545c57de6c7be08b68244633a8bcd
caio <caio_cs_goncalves@estudante.sesisenai.org.br>
2026-07-16 11:30:01 -0300

    1.3

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     // Arquivo de saÃ­da final que o navegador vai ler
     filename: 'bundle.js', 
     // O Webpack vai salvar dentro de uma pasta chamada "dist" na raiz do projeto
-    path: path.resolve(__dirname, 'dist'), 
+    path: path.resolve(__dirname, 'public'), 
   },
   // MantÃ©m o cÃ³digo legÃ­vel no bundle.js facilitando a depuraÃ§Ã£o
   mode: 'development' 
